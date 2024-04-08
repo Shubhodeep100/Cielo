f6ed0bc762a1ac73e2aadc44899b1c92807a068d
Shubhodeep <banerjeeshubho98@gmail.com>
2024-04-08 15:24:40 +0530

    Search UI

--- app/component/Input.tsx
+++ app/component/Input.tsx
@@ -5,9 +5,11 @@ const Input = () => {
 
 
   return (
-    <form className="flex items-center md:w-2/4 w-full">
-      <input type="text" placeholder="Search city" className="rounded-md w-full bg-transparent" />
-      <div className="ml-[-25px] text-white cursor-pointer "><CiSearch className="font-bold text-xl" /></div>
+    <form className="flex items-center md:w-2/4 w-full order-2 md:order-1">
+      <input type="text" placeholder="Search city" className="w-full bg-transparent border-b-2 placeholder-white outline-none text-white" />
+      <div className="ml-[-25px] text-white cursor-pointer ">
+        <CiSearch className="font-bold text-xl" />
+      </div>
     </form>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,11 @@ import Input from "./component/Input";
 const Home = () => {
   return (
     <div className="bg-cover bg-gradient-to-r from-cyan-600 to-blue-400 h-screen">
-      <div className="bg-white/25 w-full flex flex-col h-full">
+      <div className="bg-white/25 w-full flex flex-col h-fit">
         {/* Input And Logo */}
         <div className="flex flex-col justify-between items-center p-12 md:flex-row">
           <Input />
-          <h1>Cielo</h1>
+          <h1 className="mb-8 md:mb-0 order-1 text-white py-2 px-4 rounded-xl font-bold text-xl">Cielo</h1>
         </div>
       </div>
     </div>
