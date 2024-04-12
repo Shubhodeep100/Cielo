26322d05b875310417f9982c3f5dfc7359384273
Shubhodeep <banerjeeshubho98@gmail.com>
2024-04-12 12:11:24 +0530

    Merging city feature

--- app/city/[id]/page.tsx
+++ app/city/[id]/page.tsx
@@ -1,13 +1,78 @@
-import Link from "next/link"
+"use client";
+import Link from "next/link";
+import Current from "@/app/components/Current";
+import Input from "@/app/components/Input";
+import WeekForecast from "@/app/components/WeekForecast";
+import WeatherDetails from "@/app/components/WeatherDetails";
+import { useState } from "react";
 
-function City() {
+
+export default function City() {
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState("");
+  const [error, setError] = useState("");
+
+  const url = `https://api.weatherapi.com/v1/forecast.json?key=538023bd3c43455084733202231905&q=${location}&days=7&aqi=yes&alerts=yes`;
+
+  const handleSearch = async (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error();
+        }
+        const data = await response.json();
+        setData(data);
+        setLocation("");
+        setError("");
+      } catch (error) {
+        setError("City not found");
+        setData({});
+      }
+    }
+  };
+
+  let content;
+  if (Object.keys(data).length === 0 && error === "") {
+    content = (
+      <div className="text-white text-center h-screen mt-5">
+        <h2 className="text-3xl font-semibold mb-4">Welcome to Cielo</h2>
+        <p className="text-xl">Enter a city name to get the weather forecast</p>
+      </div>
+    );
+  } else if (error !== "") {
+    content = (
+      <div className="text-white text-center h-screen mt-[5rem]">
+        <h2 className="text-3xl font-semibold mb-4">City not found</h2>
+        <p className="text-xl">Please enter a valid city name</p>
+      </div>
+    );
+  } else {
+    content = (
+      <>
+        <div className="flex md:flex-row flex-col p-12 items-center justify-between mt-[-4rem] gap-8">
+          <Current data={data} />
+          <WeekForecast data={data} />
+        </div>
+        <div>
+          <WeatherDetails data={data} />
+        </div>
+      </>
+    );
+  }
+
   return (
-      <div className="bg-gradient-to-t from-slate-900 to-black bg-cover h-screen flex items-center justify-center">
-      <Link href="/" className="bg-zinc-900 rounded-xl border">
-        <div className="p-2 text-center text-white">Go back Home</div>
-      </Link>
+    <div className="bg-cover bg-gradient-to-t from-gray-800 to-black h-fit">
+      <div className=" w-full rounded-lg flex flex-col h-fit">
+        <div className="flex flex-col md:flex-row justify-between items-center px-10 mt-20">
+          <Input handleSearch={handleSearch} setLocation={setLocation}/> 
+          <Link href="/" className="bg-zinc-900 rounded-xl border-2">
+            <div className="p-2 text-center text-white">Go back</div>
+          </Link>
+        </div>
+        {content}
+      </div>
     </div>
-  )
+  );
 }
-
-export default City
---
+++ app/components/Input.tsx
@@ -0,0 +1,26 @@
+"use client";
+// import SearchIcon from "@mui/icons-material/Search";
+import { CiSearch } from "react-icons/ci";
+interface InputProps {
+    handleSearch: (event: React.KeyboardEvent<HTMLInputElement>) => void;
+    setLocation: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const Input = ({ handleSearch, setLocation }: InputProps) => {
+    return (
+        <form className="flex items-center md:w-2/4 w-full order-2 md:order-1">
+            <input
+                type="text"
+                className="w-full rounded-xl p-2 outline-none  text-black"
+                placeholder="Search city..."
+                onKeyDown={handleSearch}
+                onChange={(e) => setLocation(e.target.value)}
+            />
+            <div className="ml-[-30px] text-black cursor-pointer">
+                <CiSearch className="text-black text-xl"/>
+            </div>
+        </form>
+    );
+};
+
+export default Input;
